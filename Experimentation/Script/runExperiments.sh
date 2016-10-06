19b2cb2b6975d493495ffe6fdf5dbabe81493f8c
Debashis Ganguly <DebashisGanguly@lw2-wireless-pittnet-43-31.wireless.pitt.edu>
2016-10-06 13:39:33 -0400

    Add fault percent as part of run experiment script

--- Experimentation/Script/runExperiments.sh
+++ Experimentation/Script/runExperiments.sh
@@ -55,81 +55,86 @@ do
                     cp $CONF/mapred-site.xml_${V}_${N}${I} $HADOOP/conf/mapred-site.xml
                     cp $CONF/slaves_${V}_${N} $HADOOP/conf/slaves
 
-                    if [ "$V" == "DCRFT" ];
-                    then
-                         DATA=$PERM_STORE/data/43
-                    else
-                         DATA=$PERM_STORE/data/129
-                    fi
-
-                    for BENCH in ${BENCHMARKS};
+                    for F in ${FAULT};
                     do
-                         GRIDMIX_HOME=$HADOOP/src/benchmarks/gridmix2
-                         cd $GRIDMIX_HOME
-                         #echo "/<name>.*\.smallJobs.numOfJobs<\/name>/c\<name>'${BENCH}'.smallJobs.numOfJobs<\/name> gridmix_config.xml"
-                         sed -i '/<name>.*smallJobs\.numOfJobs<\/name>/c\<name>'${BENCH}'\.smallJobs\.numOfJobs<\/name>' gridmix_config.xml
+                         sed -i '/<name>\mapred\.map\.tasks\.fault\.percent<\/name>/!b;n;c<value>'${F}'</value>' $HADOOP/conf/mapred-site.xml
 
-                         for FILE in ${FILES};
+                         if [ "$V" == "DCRFT" ];
+                         then
+                              DATA=$PERM_STORE/data/43
+                         else
+                              DATA=$PERM_STORE/data/129
+                         fi
+
+                         for BENCH in ${BENCHMARKS};
                          do
                               GRIDMIX_HOME=$HADOOP/src/benchmarks/gridmix2
                               cd $GRIDMIX_HOME
-                              #echo "/<value>\/gridmix\/data.*<\/value>/c\<value>\/gridmix\/${FILE}<\/value> gridmix_config.xml"
-                              sed -i '/<value>\/gridmix\/data.*<\/value>/c\<value>\/gridmix\/data'${FILE}'<\/value>' gridmix_config.xml
+                              #echo "/<name>.*\.smallJobs.numOfJobs<\/name>/c\<name>'${BENCH}'.smallJobs.numOfJobs<\/name> gridmix_config.xml"
+                              sed -i '/<name>.*smallJobs\.numOfJobs<\/name>/c\<name>'${BENCH}'\.smallJobs\.numOfJobs<\/name>' gridmix_config.xml
 
-                              for RUN in ${RUNS};
+                              for FILE in ${FILES};
                               do
-
-                                  cd $HADOOP/bin
-                                  ./stop-all.sh
-                                  rm -rf $HADOOP/logs/*
-                                  rm -rf $TMP/*
-                                  ./hadoop namenode -format
-
-                                   MASTER="r653"
-                                   USERNAME="ganguly"
-                                   HOSTS="r655 r656 r657 r659 r660 r661 r662 r676"
-                                   #HOSTS="r655 r656 r657 r659 r660 r661 r662 r675 r676"
-
-                                   SWEEP="if [ -d $TMP ]; then rm -rf $TMP/*; else mkdir $TMP; fi"
-                                   SCP="scp -r $USERNAME@$MASTER:$HADOOP $HADOOP"
-                                   SCP_CONF="scp -r $USERNAME@$MASTER:$HADOOP/conf/\{mapred-site.xml,slaves} $HADOOP/conf/"
-                                   SED="sed -i '/export JAVA_HOME/c\export JAVA_HOME='\$JAVA_HOME'' $HADOOP/conf/hadoop-env.sh"
-
-                                   for HOSTNAME in ${HOSTS};
-                                   do
-                                       (ssh -l ${USERNAME} ${HOSTNAME} "hostname; pwd; ${SWEEP}; if [ -d $HADOOP ]; then ${SCP_CONF}; else ${SCP}; fi; hostname; ls; ${SED};") &
-                                   done
-                                   wait
-				   
-                                   ./start-dfs.sh;
-				                   wait;
-                                   ./start-mapred.sh;
-                                   wait;
-                                   ./hadoop dfs -copyFromLocal ${DATA}/${FILE}/part-00000 /gridmix/data${FILE}/SortUncompressed/part-00000;
-	                               wait;
-                                   #echo "hadoop dfs -copyFromLocal ${DATA}/${FILE}/part-00000 /gridmix/data${FILE}/SortUncompressed/part-00000"
-
                                    GRIDMIX_HOME=$HADOOP/src/benchmarks/gridmix2
-                                   cd $GRIDMIX_HOME;
-                                   ./rungridmix_2 | tee ${LOG}/${VER}_${BENCH}_${N}${I}_m${FILE}_r${RUN}
-                                   TIME=`sed -n '/ExecutionTime:/p' ${LOG}/${VER}_${BENCH}_${N}${I}_m${FILE}_r${RUN} | sed -r 's/([^0-9]*([0-9]*)){1}.*/\2/'`
-                                   echo "${TIME}_${VER}_${BENCH}_${N}${I}_m${FILE}_r${RUN}" >> ${LOG}/dump
+                                   cd $GRIDMIX_HOME
+                                   #echo "/<value>\/gridmix\/data.*<\/value>/c\<value>\/gridmix\/${FILE}<\/value> gridmix_config.xml"
+                                   sed -i '/<value>\/gridmix\/data.*<\/value>/c\<value>\/gridmix\/data'${FILE}'<\/value>' gridmix_config.xml
 
-                                   STAT_LOG=`find ${HADOOP}/logs/hadoop-${USERNAME}-jobtracker-*.log`
-
-                                   for S in ${STATISTICS};
+                                   for RUN in ${RUNS};
                                    do
-                                        S_TIME=`sed -n "/\b${S} = \b/p" "${STAT_LOG}" | sed -r "s/^(.*)(\b${S} = \b)([0-9]*)$/\3/"`
-                                        echo "${S} = ${S_TIME}" >> ${LOG}/dump
+
+                                       cd $HADOOP/bin
+                                       ./stop-all.sh
+                                       rm -rf $HADOOP/logs/*
+                                       rm -rf $TMP/*
+                                       ./hadoop namenode -format
+
+                                        MASTER="r653"
+                                        USERNAME="ganguly"
+                                        HOSTS="r655 r656 r657 r659 r660 r661 r662 r676"
+                                        #HOSTS="r655 r656 r657 r659 r660 r661 r662 r675 r676"
+
+                                        SWEEP="if [ -d $TMP ]; then rm -rf $TMP/*; else mkdir $TMP; fi"
+                                        SCP="scp -r $USERNAME@$MASTER:$HADOOP $HADOOP"
+                                        SCP_CONF="scp -r $USERNAME@$MASTER:$HADOOP/conf/\{mapred-site.xml,slaves} $HADOOP/conf/"
+                                        SED="sed -i '/export JAVA_HOME/c\export JAVA_HOME='\$JAVA_HOME'' $HADOOP/conf/hadoop-env.sh"
+
+                                        for HOSTNAME in ${HOSTS};
+                                        do
+                                            (ssh -l ${USERNAME} ${HOSTNAME} "hostname; pwd; ${SWEEP}; if [ -d $HADOOP ]; then ${SCP_CONF}; else ${SCP}; fi; hostname; ls; ${SED};") &
+                                        done
+                                        wait
+     				   
+                                        ./start-dfs.sh;
+     				                   wait;
+                                        ./start-mapred.sh;
+                                        wait;
+                                        ./hadoop dfs -copyFromLocal ${DATA}/${FILE}/part-00000 /gridmix/data${FILE}/SortUncompressed/part-00000;
+     	                               wait;
+                                        #echo "hadoop dfs -copyFromLocal ${DATA}/${FILE}/part-00000 /gridmix/data${FILE}/SortUncompressed/part-00000"
+
+                                        GRIDMIX_HOME=$HADOOP/src/benchmarks/gridmix2
+                                        cd $GRIDMIX_HOME;
+                                        ./rungridmix_2 | tee ${LOG}/${VER}_${BENCH}_${N}${I}_m${FILE}_f${F}_r${RUN}
+                                        TIME=`sed -n '/ExecutionTime:/p' ${LOG}/${VER}_${BENCH}_${N}${I}_m${FILE}_f${F}_r${RUN} | sed -r 's/([^0-9]*([0-9]*)){1}.*/\2/'`
+                                        echo "${TIME}_${VER}_${BENCH}_${N}${I}_m${FILE}_f${F}_r${RUN}" >> ${LOG}/dump
+
+                                        STAT_LOG=`find ${HADOOP}/logs/hadoop-${USERNAME}-jobtracker-*.log`
+
+                                        for S in ${STATISTICS};
+                                        do
+                                             S_TIME=`sed -n "/\b${S} = \b/p" "${STAT_LOG}" | sed -r "s/^(.*)(\b${S} = \b)([0-9]*)$/\3/"`
+                                             echo "${S} = ${S_TIME}" >> ${LOG}/dump
+                                        done
+                                        ls ${STAT_LOG};
+                                        ls ${LOG}/${VER}_${BENCH}_${N}${I}_m${FILE}_f${F}_r${RUN};
+                                        cp ${STAT_LOG} ${LOG}/${VER}_${BENCH}_${N}${I}_m${FILE}_f${F}_r${RUN}_jobtracker;
+     				   
                                    done
-                                   ls ${STAT_LOG};
-                                   ls ${LOG}/${VER}_${BENCH}_${N}${I}_m${FILE}_r${RUN};
-                                   cp ${STAT_LOG} ${LOG}/${VER}_${BENCH}_${N}${I}_m${FILE}_r${RUN}_jobtracker;
-				   
                               done
                          done
                     done
-               fi 
+               fi
           done
      done
 done
